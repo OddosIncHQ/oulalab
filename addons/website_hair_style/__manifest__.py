1f7839d4eb8d8cd145e406a03d126c898ea905a4
Oddos Solutions <145717033+OddosIncHQ@users.noreply.github.com>
2024-12-05 18:36:21 -0300

    Create __manifest__.py

---
+++ addons/website_hair_style/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Integración de Estilos de Cabello en el Sitio Web',
+    'version': '1.0',
+    'category': 'Website',
+    'summary': 'Integración de la API de YouCam Online Editor para estilos de cabello en el sitio web de Odoo',
+    'author': 'Tu Nombre',
+    'depends': ['website'],
+    'data': [
+        # Archivos de vistas o plantillas si son necesarios
+    ],
+    'installable': True,
+    'application': False,
+}
