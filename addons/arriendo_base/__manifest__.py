58c80cc0d580c98310c8496f6e3e7e367f9649f9
Oddos Solutions <145717033+OddosIncHQ@users.noreply.github.com>
2025-07-22 19:20:51 -0400

    Create __manifest__.py

---
+++ addons/arriendo_base/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Arriendo Base',
+    'version': '1.0',
+    'summary': 'Modelos base para arriendo de prendas',
+    'depends': ['base', 'stock', 'product', 'website', 'portal'],
+    'author': 'Tu Empresa',
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': False,
+}
