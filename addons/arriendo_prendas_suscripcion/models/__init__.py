799ff90fe764a03e7078911f66a3a26a042edc7b
Oddos Solutions <145717033+OddosIncHQ@users.noreply.github.com>
2025-07-22 19:05:52 -0400

    Update __init__.py

--- addons/arriendo_prendas_suscripcion/models/__init__.py
+++ addons/arriendo_prendas_suscripcion/models/__init__.py
@@ -1,6 +1,4 @@
-# -*- coding: utf-8 -*-
-# Importa todos los archivos Python dentro del directorio 'models'
 from . import arriendo_prenda_linea
 from . import product
 from . import stock_picking
-from . import suscripcion
+from . import gestion_suscripcion
