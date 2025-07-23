d75adc9e214ea2d76524c458a010629d96dd65fb
Oddos Solutions <145717033+OddosIncHQ@users.noreply.github.com>
2025-07-23 13:02:15 -0400

    Update sale_order.py

--- addons/arriendo_prendas_suscripcion/models/sale_order.py
+++ addons/arriendo_prendas_suscripcion/models/sale_order.py
@@ -1,7 +1,13 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
+
+    x_max_prendas_permitidas = fields.Integer(string='Máximo de prendas permitidas')
+    x_cambios_permitidos_mes = fields.Integer(string='Cambios permitidos por mes')
+    x_cambios_usados_mes = fields.Integer(string='Cambios usados este mes')
+    x_proxima_fecha_reseteo_cambios = fields.Date(string='Fecha de reseteo de cambios')
+    x_cantidad_prendas_en_posesion = fields.Integer(string='Cantidad actual de prendas')
 
     x_prendas_en_posesion_ids = fields.One2many(
         comodel_name='arriendo_prendas_suscripcion.arriendo_prenda_linea',
