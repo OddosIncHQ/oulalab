d35239c4f25f7c892b9c1db5d11a91eaa98b104a
Oddos Solutions <145717033+OddosIncHQ@users.noreply.github.com>
2025-07-24 16:28:59 -0400

    Update arriendo_prenda_linea.py

--- addons/arriendo_prendas_suscripcion/models/arriendo_prenda_linea.py
+++ addons/arriendo_prendas_suscripcion/models/arriendo_prenda_linea.py
@@ -1,6 +1,7 @@
 from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError
 
+
 class ArriendoPrendaLinea(models.Model):
     """
     Este es el modelo principal que rastrea cada instancia de una prenda arrendada.
