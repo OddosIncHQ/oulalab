f100a163188e9fdc34e4d450fd0b8a2406434529
Oddos Solutions <145717033+OddosIncHQ@users.noreply.github.com>
2025-07-24 14:23:45 -0400

    Update __manifest__.py

--- addons/arriendo_prendas_suscripcion/__manifest__.py
+++ addons/arriendo_prendas_suscripcion/__manifest__.py
@@ -1,9 +1,8 @@
 {
     'name': 'Arriendos por Suscripción',
-    'version': '1.2.1',
+    'version': '1.2.3', # Incrementamos la versión
     'summary': 'Aplicación para la gestión integral del modelo de arriendo de prendas.',
     'author': 'Tu Nombre/Empresa',
-    'website': 'https://www.tusitio.com',
     'category': 'Sales/Subscription',
     'depends': [
         'sale_subscription',
@@ -17,18 +16,17 @@
         'security/security.xml',
         'security/ir.model.access.csv',
 
-        # 2. Datos (como cron jobs)
+        # 2. Datos
         'data/cron_jobs.xml',
         
-        # 3. Vistas del Backend y Menús
-        # El archivo de menús debe ir primero para que las demás vistas se "ancoren" a él.
+        # 3. Vistas y Menús
         'views/menu_views.xml',
         'views/arriendo_prenda_linea_views.xml',
         'views/suscripcion_views.xml',
         'views/stock_picking_views.xml',
         'views/product_views.xml',
         
-        # 4. Plantillas del Frontend (Portal)
+        # 4. Plantillas del Portal
         'templates/portal_template.xml',
         'templates/catalogo_arriendo_template.xml',
     ],
