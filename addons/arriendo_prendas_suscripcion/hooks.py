99980ab836a2435969aa01b77dde4cb47b0434c6
Oddos Solutions <145717033+OddosIncHQ@users.noreply.github.com>
2025-07-24 10:33:10 -0400

    Update hooks.py

--- addons/arriendo_prendas_suscripcion/hooks.py
+++ addons/arriendo_prendas_suscripcion/hooks.py
@@ -1,13 +1,12 @@
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
-from odoo.api import Environment, SUPERUSER_ID
+from odoo import SUPERUSER_ID, api
 
-def post_init_hook(cr, registry):
-    """Set nextcall del cron a la próxima fecha del mes (1° del siguiente mes a las 03:00)"""
-    env = Environment(cr, SUPERUSER_ID, {})
+def post_init_hook(env):
+    """Setea la próxima ejecución del cron al 1° del mes siguiente a las 03:00"""
     cron = env.ref('arriendo_prendas_suscripcion.ir_cron_reset_monthly_subscription_changes', raise_if_not_found=False)
-
     if cron:
         now = datetime.now()
-        first_day_next_month = (now + relativedelta(months=1)).replace(day=1, hour=3, minute=0, second=0, microsecond=0)
-        cron.nextcall = first_day_next_month.strftime('%Y-%m-%d %H:%M:%S')
+        next_month = now + relativedelta(months=1)
+        nextcall = next_month.replace(day=1, hour=3, minute=0, second=0, microsecond=0)
+        cron.nextcall = nextcall.strftime('%Y-%m-%d %H:%M:%S')
