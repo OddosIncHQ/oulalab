a114cb0208aa6876e52f5393b854331c2a02adba
Oddos Solutions <145717033+OddosIncHQ@users.noreply.github.com>
2025-07-24 09:48:36 -0400

    Update sale_order.py

--- addons/arriendo_prendas_suscripcion/models/sale_order.py
+++ addons/arriendo_prendas_suscripcion/models/sale_order.py
@@ -1,4 +1,7 @@
 from odoo import models, fields, api
+from datetime import date
+from dateutil.relativedelta import relativedelta
+
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
@@ -14,3 +17,27 @@ class SaleOrder(models.Model):
         inverse_name='suscripcion_id',
         string='Prendas Arrendadas'
     )
+
+    @api.model
+    def action_reset_monthly_changes(self):
+        """Resetea los cambios usados en el mes para las suscripciones activas."""
+        hoy = date.today()
+        ordenes = self.search([
+            ('x_proxima_fecha_reseteo_cambios', '<=', hoy)
+        ])
+        for orden in ordenes:
+            nueva_fecha = (hoy + relativedelta(months=1)).replace(day=1)
+            orden.write({
+                'x_cambios_usados_mes': 0,
+                'x_proxima_fecha_reseteo_cambios': nueva_fecha,
+            })
+            _logger = self.env['ir.logging']
+            _logger.create({
+                'name': 'Reset mensual',
+                'type': 'server',
+                'level': 'INFO',
+                'message': f"Reset mensual aplicado a {orden.name}",
+                'path': 'sale.order',
+                'func': 'action_reset_monthly_changes',
+                'line': 0,
+            })
