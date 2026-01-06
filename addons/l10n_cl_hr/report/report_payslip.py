51a7a566df2f9370e397bbca78f65164989fc615
Oddos Solutions <145717033+OddosIncHQ@users.noreply.github.com>
2026-01-06 14:42:07 -0300

    Update report_payslip.py

--- addons/l10n_cl_hr/report/report_payslip.py
+++ addons/l10n_cl_hr/report/report_payslip.py
@@ -1,44 +1,40 @@
+# -*- coding: utf-8 -*-
 from odoo import api, models
+from .amount_to_text_es import amount_to_text
 
-
-class payslip_report(models.AbstractModel):
-    _inherit = 'report.hr_payroll.report_payslipdetails'
+class PayslipReport(models.AbstractModel):
+    # Este nombre vincula la clase con el ID del template XML 'hr_payroll.report_payslip'
+    _name = 'report.hr_payroll.report_payslip'
+    _description = 'Reporte de Liquidación de Sueldo'
 
     @api.model
     def _get_report_values(self, docids, data=None):
-        payslips = super(payslip_report, self)._get_report_values(docids, data)
-        #payslips.update({
-#        	'get_payslip_lines': self.get_payslip_lines(),
-#            #'convert': self.convert(),
-#            'get_leave':self.get_leave(),
-#        })
-        return payslips
+        """
+        Sobrescribe la generación de valores para el reporte.
+        Aquí inyectamos funciones personalizadas para usar en el QWeb.
+        """
+        # Obtenemos los objetos de nómina seleccionados
+        payslips = self.env['hr.payslip'].browse(docids)
 
-    @api.multi
-    def convert(self,amount, cur):
-        amt_en = cur.amount_to_text(amount)
-        return amt_en
+        return {
+            'doc_ids': docids,
+            'doc_model': 'hr.payslip',
+            'docs': payslips,
+            'data': data,
+            'company_id': self.env.company,
+            
+            # Inyectamos la función personalizada de conversión a texto
+            # En el XML se usa como: <span t-esc="convert(o.net_wage, o.currency_id)"/>
+            'convert': self._convert_to_text,
+        }
 
-    @api.multi
-    def get_payslip_lines(self):
-        payslip_line = self.env['hr.payslip.line']
-        res = []
-        ids = []
-        for rec in self:
-            if rec.appears_on_payslip is True:
-                ids.append(rec.id)
-        if ids:
-            res = payslip_line.browse(ids)
-        return res
-
-    @api.multi
-    def get_leave(self, obj):
-        res = []
-        ids = []
-        for rec in self:
-            if rec.type == 'leaves':
-                ids.append(rec.id)
-            payslip_line = self.env['hr.payslip.line']
-            if ids:
-                res = payslip_line.browse(ids)
-        return res
+    def _convert_to_text(self, amount, currency):
+        """
+        Función helper para convertir montos a palabras usando 
+        la librería personalizada 'amount_to_text_es.py'
+        """
+        if not amount:
+            return ""
+            
+        currency_name = currency.name if currency else 'CLP'
+        return amount_to_text(amount, 'es', currency_name)
