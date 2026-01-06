f97b565c7895a792bf26eee92036bb94274b8c8e
Oddos Solutions <145717033+OddosIncHQ@users.noreply.github.com>
2026-01-06 12:51:09 -0300

    Update hr_ccaf.py

--- addons/l10n_cl_hr/model/hr_ccaf.py
+++ addons/l10n_cl_hr/model/hr_ccaf.py
@@ -1,8 +1,13 @@
+# -*- coding: utf-8 -*-
 from odoo import models, fields
 
 class HrCCAF(models.Model):
     _name = 'hr.ccaf'
-    _description = 'Caja de Compensación'
+    _description = 'Cajas de Compensación de Asignación Familiar (CCAF)'
+    _order = 'name asc'
 
     name = fields.Char(string='Nombre', required=True)
-    code = fields.Char(string='Código')
+    code = fields.Char(string='Código', help="Código oficial para archivo de Previred")
+    
+    # Campo estándar para archivar registros
+    active = fields.Boolean(default=True, string="Activo")
