3003bdadd8d4e5eeaf26519b86f2e7a26e02097d
Oddos Solutions <145717033+OddosIncHQ@users.noreply.github.com>
2026-01-06 08:23:04 -0300

    Update hr_employee.py

--- addons/l10n_cl_hr/model/hr_employee.py
+++ addons/l10n_cl_hr/model/hr_employee.py
@@ -1,15 +1,13 @@
 import re
 import logging
-
 from odoo import models, fields, api
 from odoo.exceptions import UserError
 
 _logger = logging.getLogger(__name__)
 
-
 class HrEmployee(models.Model):
     _inherit = 'hr.employee'
-    
+
     firstname = fields.Char("Firstname")
     last_name = fields.Char("Last Name")
     middle_name = fields.Char("Middle Name", help='Employees middle name')
@@ -39,7 +37,7 @@ class HrEmployee(models.Model):
     def onchange_document(self):
         identification_id = (
             re.sub('[^1234567890Kk]', '',
-            str(self.identification_id))).zfill(9).upper()
+                   str(self.identification_id))).zfill(9).upper()
         self.identification_id = '%s.%s.%s-%s' % (
             identification_id[0:2], identification_id[2:5], identification_id[5:8],
             identification_id[-1])
@@ -47,16 +45,17 @@ class HrEmployee(models.Model):
     def check_identification_id_cl(self, identification_id):
         body, vdig = '', ''
         if len(identification_id) > 9:
-            identification_id = identification_id.replace('-','',1).replace('.','',2)
+            identification_id = identification_id.replace('-', '', 1).replace('.', '', 2)
         if len(identification_id) != 9:
             raise UserError(u'El Rut no tiene formato')
         else:
             body, vdig = identification_id[:-1], identification_id[-1].upper()
         try:
-            vali = range(2,8) + [2,3]
+            # ✅ FIX para Python 3:
+            vali = list(range(2, 8)) + [2, 3]
             operar = '0123456789K0'[11 - (
-                sum([int(digit)*factor for digit, factor in zip(
-                    body[::-1],vali)]) % 11)]
+                sum([int(digit) * factor for digit, factor in zip(
+                    body[::-1], vali)]) % 11)]
             if operar == vdig:
                 return True
             else:
@@ -76,3 +75,4 @@ class HrEmployee(models.Model):
                 ])
             if r.identification_id != "55.555.555-5" and employee:
                 raise UserError(u'El Rut debe ser único')
+
