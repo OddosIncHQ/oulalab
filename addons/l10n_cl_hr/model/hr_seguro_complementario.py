f9811fcd6d64c1b92cacaad6fa29cae14b30e6e9
Oddos Solutions <145717033+OddosIncHQ@users.noreply.github.com>
2026-01-06 13:20:36 -0300

    Update hr_seguro_complementario.py

--- addons/l10n_cl_hr/model/hr_seguro_complementario.py
+++ addons/l10n_cl_hr/model/hr_seguro_complementario.py
@@ -1,8 +1,16 @@
-from odoo import api, fields, models, _
+# -*- coding: utf-8 -*-
+from odoo import models, fields
 
 class HrSeguroComplementario(models.Model):
     _name = 'hr.seguro.complementario'
-    _description = 'Seguro Complementario'
+    _description = 'Seguro Complementario de Salud'
+    _order = 'name asc'
 
-    codigo = fields.Char('Código', required=True)
-    name = fields.Char('Nombre', required=True)
+    name = fields.Char(string='Nombre Aseguradora', required=True)
+    code = fields.Char(string='Código Interno', required=True)
+    
+    # Útil para reportes o identificación oficial
+    rut = fields.Char(string='RUT Aseguradora', help="RUT de la compañía de seguros")
+    
+    # Campo estándar para archivar
+    active = fields.Boolean(default=True, string="Activo")
