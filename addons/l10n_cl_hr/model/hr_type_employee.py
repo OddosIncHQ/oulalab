b2f3ae63463140e50e9e32eb8a3f01e951cdde2b
Oddos Solutions <145717033+OddosIncHQ@users.noreply.github.com>
2026-01-06 13:21:43 -0300

    Update hr_type_employee.py

--- addons/l10n_cl_hr/model/hr_type_employee.py
+++ addons/l10n_cl_hr/model/hr_type_employee.py
@@ -1,9 +1,15 @@
-from odoo import api, fields, models, _
-
+# -*- coding: utf-8 -*-
+from odoo import models, fields
 
 class HrTypeEmployee(models.Model):
     _name = 'hr.type.employee'
-    _description = 'Tipo de Empleado'
+    _description = 'Tipo de Trabajador (Código Previred)'
+    _order = 'id_type asc'
 
-    id_type = fields.Char(string='Código', required=True)
-    name = fields.Char(string='Nombre', required=True)
+    # Código oficial para el archivo de Previred (ej: 0=Activo, 1=Pensionado, etc.)
+    id_type = fields.Char(string='Código Previred', required=True)
+    
+    name = fields.Char(string='Descripción', required=True)
+    
+    # Campo estándar para archivar en Odoo 19
+    active = fields.Boolean(default=True, string="Activo")
