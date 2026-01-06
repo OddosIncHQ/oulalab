d2a82300821165d3447be34a27f0530731d47f65
Oddos Solutions <145717033+OddosIncHQ@users.noreply.github.com>
2026-01-06 14:47:31 -0300

    Update hr_contract_type.py

--- addons/l10n_cl_hr/model/hr_contract_type.py
+++ addons/l10n_cl_hr/model/hr_contract_type.py
@@ -3,10 +3,11 @@ from odoo import models, fields
 
 class HrContractType(models.Model):
     _inherit = 'hr.contract.type'
-    _description = 'Tipo de Contrato Laboral'
+    _description = 'Tipo de Contrato Laboral (Chile)'
 
-    # Campo utilizado para mapear con los códigos de Previred / Dirección del Trabajo
-    codigo = fields.Char(
-        string='Código Oficial', 
-        help="Código numérico para archivos de Previred (ej: 1 para Indefinido)"
+    # Campo utilizado para mapear con los códigos de Previred
+    # (Ej: 1=Indefinido, 7=Plazo Fijo, 1=Sueldo Empresarial)
+    l10n_cl_code = fields.Char(
+        string='Código Previred', 
+        help="Código numérico para archivos de Previred (ej: 1, 7, etc.)"
     )
