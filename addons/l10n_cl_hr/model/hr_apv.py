57c702a11d68e364db1c5d10144a6757b8f4cde2
Oddos Solutions <145717033+OddosIncHQ@users.noreply.github.com>
2026-01-06 12:50:10 -0300

    Update hr_apv.py

--- addons/l10n_cl_hr/model/hr_apv.py
+++ addons/l10n_cl_hr/model/hr_apv.py
@@ -1,8 +1,13 @@
+# -*- coding: utf-8 -*-
 from odoo import models, fields
 
 class HrAPV(models.Model):
     _name = 'hr.apv'
-    _description = 'Institución APV'
+    _description = 'Instituciones de Ahorro Previsional Voluntario (APV)'
+    _order = 'name asc'
 
     name = fields.Char(string='Nombre', required=True)
-    code = fields.Char(string='Código')
+    code = fields.Char(string='Código', help="Código oficial para archivo de Previred")
+    
+    # Permite archivar la institución sin eliminarla de la base de datos
+    active = fields.Boolean(default=True, string="Activo")
