38923713bd0b5200385c9cc1ed49ef8c375539b2
Oddos Solutions <145717033+OddosIncHQ@users.noreply.github.com>
2026-01-06 12:40:52 -0300

    Update hr_afp.py

--- addons/l10n_cl_hr/model/hr_afp.py
+++ addons/l10n_cl_hr/model/hr_afp.py
@@ -1,13 +1,10 @@
-from odoo import api, fields, models, _
-
+from odoo import models, fields
 
 class HrAFP(models.Model):
     _name = 'hr.afp'
-    _description = 'Fondos de Pensiones'
+    _description = 'AFP'
 
-    codigo = fields.Char(string='Código', required=True)
     name = fields.Char(string='Nombre', required=True)
-    rut = fields.Char(string='RUT', required=True)
-    rate = fields.Float(string='Tasa', required=True, help="Tasa de cotización obligatoria")
-    sis = fields.Float(string='Aporte Empresa', required=True, help="Seguro de Invalidez y Sobrevivencia")
-    independiente = fields.Float(string='Independientes', required=True, help="Tasa para trabajadores independientes")
+    code = fields.Char(string='Código')
+    rate = fields.Float(string='Tasa (%)', digits=(16, 2))
+    sis = fields.Float(string='SIS (%)', digits=(16, 2), help='Seguro de Invalidez y Sobrevivencia')
