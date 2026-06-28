9eb1739c4f4ed8fe318eb9a114d672c0869e79a8
Oddos Solutions <145717033+OddosIncHQ@users.noreply.github.com>
2026-06-27 22:14:03 -0400

    Actualizar App.tsx

--- webpage/App.tsx
+++ webpage/App.tsx
@@ -242,8 +242,8 @@ const StandaloneWaitlist: React.FC<{ lang: Language }> = ({ lang }) => {
             const telefono = (form.elements.namedItem('telefono') as HTMLInputElement).value;
             const address = (form.elements.namedItem('address') as HTMLInputElement).value;
             try {
-              // URL ACTUALIZADA A LA VERSIÓN 8 (Termina en PAP95BcNsP)
-              await fetch("https://script.google.com/macros/s/AKfycbxMc3pnCBMmzHuwpkeakGJgqkmax-me2RgDizu9HFIxRQyj5e4BHIgSp1PAP95BcNsP/exec", {
+              // URL ACTUALIZADA A LA VERSIÓN 10 (Termina en LttVbRz)
+              await fetch("https://script.google.com/macros/s/AKfycbzH_YPxIGHNlqptESWS_sbftxUR0iWx9iJTJQJ_6tc5I4H_GYcpWokMQcARLttVbRz/exec", {
                 method: "POST", mode: 'no-cors', headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ nombre, email, telefono, comuna: address })
               });
@@ -786,8 +786,8 @@ const App: React.FC = () => {
                 const telefono = (form.elements.namedItem('telefono') as HTMLInputElement).value;
                 const address = (form.elements.namedItem('address') as HTMLInputElement).value;
                 try {
-                  // URL ACTUALIZADA A LA VERSIÓN 8 (Termina en PAP95BcNsP)
-                  await fetch("https://script.google.com/macros/s/AKfycbxMc3pnCBMmzHuwpkeakGJgqkmax-me2RgDizu9HFIxRQyj5e4BHIgSp1PAP95BcNsP/exec", {
+                  // URL ACTUALIZADA A LA VERSIÓN 10 (Termina en LttVbRz)
+                  await fetch("https://script.google.com/macros/s/AKfycbzH_YPxIGHNlqptESWS_sbftxUR0iWx9iJTJQJ_6tc5I4H_GYcpWokMQcARLttVbRz/exec", {
                     method: "POST", mode: 'no-cors', headers: { "Content-Type": "application/json" },
                     body: JSON.stringify({ nombre, email, telefono, comuna: address })
                   });
